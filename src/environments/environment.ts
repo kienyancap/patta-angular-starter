dfeba54fdfb9b4d5da93aadb471b75b143e8fc0c
Kien Yan <kienyanneko@gmail.com>
2023-07-12 10:23:26 +0200

    Configure environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,14 @@
 export const environment = {
-  production: true,
+  production: false,
 };
 
 export const Config = {
-  api_key: '',
-  delivery_token: '',
-  environment: '',
-  region: '',
-  management_token: '',
-  api_host: '',
-  app_host: '',
+  api_key: 'blt15e15549dfce3446',
+  delivery_token: 'csce0909eb0cbef28c0e4b83a6',
+  environment: 'development',
+  region: 'eu',
+  management_token: 'cs6c919e04be64a162ac3cf1f6',
+  api_host: 'eu-api.contentstack.com',
+  app_host: 'app.contentstack.com',
   live_preview: true,
 };
